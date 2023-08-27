240b48be1219d8b775dff546cc98f57d9dfe338b
Ira S <iryna.shevchenko2@gmail.com>
2023-08-27 23:08:44 +0300

    in pr

--- src/js/battle-ships.js
+++ src/js/battle-ships.js
@@ -1,36 +1,54 @@
-// const randomLoc = Math.floor(Math.random() * 5);
+const view = {
+    displayMessage: function (msg) {
+        const messageArea = document.getElementById("messageArea");
+        messageArea.innerHTML = msg;
+    },
+    displayHit: function (location) {
+        const cell = document.getElementById(location);
+        cell.setAttribute("class", "hit");
+    },
+    displayMiss: function (location) {
+        const cell = document.getElementById(location);
+        cell.setAttribute("class", "miss");
+    },
+    isSunk: function (ship) {
+        for (i = 0; i < this.ship; i++) {
+            if (ship.hits[i] !== "hit") {
+                return false;
+            }
+        }
+        return true;
+    }
+};
 
-// const location1 = randomLoc;
-// let location2 = location1 + 1;
-// let location3 = location2 + 1;
 
-// let guess = undefined;
-// let hits = 0;
-// let guesses = 0;
-
-// let isSunk = false;
-
-// while (isSunk == false) {
-//     guess = prompt("Ready, aim, fire! (enter a number from 0-6):");
-    
-//     if (guess < 0 || guess > 6) {
-//         alert("Please enter a valid cell number!");
-//     } else {
-//         guesses += 1;
-
-//         if (guess == location1 || guess == location2 || guess == location3) {
-//         hits += 1;
-//             alert("HIT!");
-        
-//             if (hits == 3) {
-//         isSunk = true;
-//             alert("You sunk my battleship!");
-//         };
-//         } else {
-//         alert("MISS!");
-//     };
-//     };
-//     }
-
-// const stats = "You took " + guesses + "guesses to sink the battleship, " + "which means you shooting accuracy was " + (3 / guesses);
-// alert(stats);
+const model = {
+    boardSize: 7,
+    numShips: 3,
+    shipLength: 3,
+    ships: [
+        { locations: ["06", "16", "26"], hits: ["", "", "",] },
+        { locations: ["24", "34", "44"], hits: ["", "", "",] },
+        { locations: ["10", "11", "12"], hits: ["", "", "",] },
+    ],
+    fire: function (guess) {
+        for (i = 0; i < this.numShips; i++){
+            const ship = this.ships[i];
+            const index = ship.locations.indexOf(guess);
+            if (index >= 0) {
+                ship.hits[index] = "hit";
+                view.displayHit(guess);
+                view.displayMessage("HIT!");
+                if (this.isSunk(ship)) {
+                    view.displayMessage("You sank my battleship!")
+                    this.shipSunk++;
+                }
+                return true;
+            }
+        }
+        view.displayMiss(guess);
+        view.displayMessage("You missed.")
+        return false;
+    },
+    isSunk: function(ship) {},
+};
