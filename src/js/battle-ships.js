ae0714e4f5dcc00c73b41f1350d15117391e6253
Ira S <iryna.shevchenko2@gmail.com>
2023-08-28 23:19:12 +0300

    in pr

--- src/js/battle-ships.js
+++ src/js/battle-ships.js
@@ -53,4 +53,32 @@ const model = {
     },
 };
 
+const controller = {
+    guesses: 0,
 
+    processGuess: function (guess) {
+        
+    }
+};
+
+function parseGuess(guess) {
+    const alphabet = ["A", "B", "C", "D", "E", "F", "G"];
+
+    if (guess === null || guess.length !== 2) {
+            alert("Oops, please enter a letter and a number on the board.");
+        } else {
+            const firstChar = guess.charAt(0);
+     }
+}
+
+// model.fire("53");
+// model.fire("16");
+// model.fire("26");
+
+// model.fire("34");
+// model.fire("24");
+// model.fire("44");
+
+// model.fire("12");
+// model.fire("11");
+// model.fire("10");
