448f290851ce5d5ff69037b34d2393441be68a2d
Ira S <iryna.shevchenko2@gmail.com>
2023-08-30 00:22:17 +0300

    FIXED

--- src/js/battle-ships.js
+++ src/js/battle-ships.js
@@ -88,28 +88,28 @@ function parseGuess(guess) {
     return null;
 }
 
-function handleFireButton() {
-    const guessInput = document.getElementById("guessInput");
-    const guess = guessInput.value;
-    controller.processGuess(guess);
-    guessInput.value = "";
-}
+// function handleFireButton() {
+//     const guessInput = document.getElementById("guessInput");
+//     const guess = guessInput.value;
+//     controller.processGuess(guess);
+//     guessInput.value = "";
+// }
 
-function init() {
-    const fireButton = document.getElementById("fireButton");
-    fireButton.onclick = handleFireButton;
-}
+// function init() {
+//     const fireButton = document.getElementById("fireButton");
+//     fireButton.addEventListener("click", handleFireButton);
+// }
 
-window.onload = init;
+// window.onload = init;
 
 controller.processGuess("A6");
 controller.processGuess("B6");
+
 controller.processGuess("C6");
 
 controller.processGuess("C4");
+
 controller.processGuess("D4");
+
 controller.processGuess("E4");
 
-controller.processGuess("B0");
-controller.processGuess("B1");
-controller.processGuess("B2");
